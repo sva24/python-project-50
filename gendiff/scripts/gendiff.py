690a57188137827c74aa3ddfa9f94bfdaca2243c
sva24 <sva@itx24.ru>
2024-06-24 12:51:01 +0700

    add -f command

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,6 +4,7 @@ import argparse
 def main():
 
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('-f', '--format', help='set format of output')
     parser.add_argument('str', metavar='first file', type=str)
     parser.add_argument('str', metavar='second file', type=str)
     args = parser.parse_args()
