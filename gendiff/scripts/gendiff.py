4c6d7aadcafa2f914772261903d091282426c9cf
sva24 <sva@itx24.ru>
2024-06-24 12:17:55 +0700

    add gendiff help and makefile

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('str', metavar='first file', type=str)
+    parser.add_argument('str', metavar='second file', type=str)
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
