73d881a86a39c423762c2ec57bb1af2cb48cf293
sva24 <sva@itx24.ru>
2024-06-25 18:00:25 +0700

    fix test paths

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,12 +1,13 @@
-import pytest
-import json
+import os
 from gendiff import generate_diff
 
 
 def test_flat_json():
-    with open('tests\\fixtures\\expectation.txt', "r") as file:
-        expectation = file.read()
-    file1 = 'tests\\fixtures\\file1.json'
-    file2 = 'tests\\fixtures\\file2.json'
+    expectation_path = os.path.join('tests', 'fixtures', 'expectation.txt')
+    file1_path = os.path.join('tests', 'fixtures', 'file1.json')
+    file2_path = os.path.join('tests', 'fixtures', 'file2.json')
 
-    assert generate_diff(file1, file2) == expectation
+    with open(expectation_path, "r") as file:
+        expectation = file.read()
+
+    assert generate_diff(file1_path, file2_path) == expectation
