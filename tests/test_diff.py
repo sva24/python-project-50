5a9d198b4acb48afc533f3d50e11515e1a09ddf7
sva24 <sva@itx24.ru>
2024-06-25 15:57:23 +0700

    rework test

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -3,47 +3,10 @@ import json
 from gendiff import generate_diff
 
 
-@pytest.fixture
-def files(tmpdir):
-    file1 = {
-        "host": "hexlet.io",
-        "timeout": 50,
-        "proxy": "123.234.53.22",
-        "follow": False
-    }
-    file2 = {
-        "timeout": 20,
-        "verbose": True,
-        "host": "hexlet.io"
-    }
+def test_flat_json():
+    with open('tests\\fixtures\\expectation.txt', "r") as file:
+        expectation = file.read()
+    file1 = 'tests\\fixtures\\file1.json'
+    file2 = 'tests\\fixtures\\file2.json'
 
-    file1_path = tmpdir.join("file1.json")
-    file2_path = tmpdir.join("file2.json")
-
-    with open(file1_path, "w") as f:
-        json.dump(file1, f)
-
-    with open(file2_path, "w") as f:
-        json.dump(file2, f)
-
-    return str(file1_path), str(file2_path)
-
-
-def test_flat_json(files):
-    assert generate_diff(*files) == (
-        "{\n"
-        "  - follow: False\n"
-        "    host: hexlet.io\n"
-        "  - proxy: 123.234.53.22\n"
-        "  - timeout: 50\n"
-        "  + timeout: 20\n"
-        "  + verbose: True\n"
-        "}"
-    )
-    f1, f2 = files
-    assert generate_diff(f1, f1) == ('{\n'
-                                     '    follow: False\n'
-                                     '    host: hexlet.io\n'
-                                     '    proxy: 123.234.53.22\n'
-                                     '    timeout: 50\n'
-                                     '}')
+    assert generate_diff(file1, file2) == expectation
