dbe34dd43cee73193c5718120de90a6155386654
sva24 <sva@itx24.ru>
2024-07-04 12:21:53 +0700

    some fix

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,6 +1,5 @@
-from .generate.generate_flat import generate_diff
 from .formatters.stylish import make_stylish
-from .generate.generate_nested import build_diff
+from .generate.generate import build_diff
 from gendiff.files_loader import load_files
 from .formatters.plain import make_plain
 from .formatters.json import make_json
